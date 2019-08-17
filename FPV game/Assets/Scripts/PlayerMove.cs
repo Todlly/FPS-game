04b7364251787359dea0a68a4a1b87659ea6d7b2
Sergey <furrykunstler@gmail.com>
2019-08-17 21:24:12 +0300

    Let the player move

--- FPV game/Assets/Scripts/PlayerMove.cs
+++ FPV game/Assets/Scripts/PlayerMove.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody player;
+    public float speed = 10f;
+
     void Start()
     {
-        
+        player = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(h, 0f, v);
+        movement = movement.normalized * speed * Time.deltaTime;
+        player.MovePosition(transform.position + movement);
     }
 }
