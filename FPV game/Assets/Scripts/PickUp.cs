63040d7b77730e45ba7d6e6efa5f314474984908
Sergey <furrykunstler@gmail.com>
2019-08-20 20:13:19 +0300

    Trying implement selection

---
+++ FPV game/Assets/Scripts/PickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : Interactable
+{
+    public Material defaultMaterial;
+    public Material highlightedMaterial;
+    private Material material;
+
+    private void Awake()
+    {
+        material = GetComponent<Material>();
+    }
+
+    public override void Highlight()
+    {
+        material = highlightedMaterial;
+    }
+
+    public override void DeHighlight()
+    {
+        material = defaultMaterial;
+    }
+}
---
+++ FPV game/Assets/Scripts/Selector.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Selector : MonoBehaviour
+{
+    Ray ray;
+
+    private void Awake()
+    {
+    }
+
+    private void Update()
+    {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (hit.transform.gameObject.CompareTag("Interactable"))
+            {
+                Debug.Log("HitInter");
+                PickUp obj = hit.transform.GetComponent<PickUp>();
+                obj.Highlight();
+            }
+        }
+    }
+}
